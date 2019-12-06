dfe355767363a51a4136075b23c94aa8f36899b6
sashokdzr <sashok200888@gmail.com>
2019-12-06 23:35:18 +0300

    # Conflicts: #	src/Trainee.java

--- src/Trainee.java
+++ src/Trainee.java
@@ -2,5 +2,6 @@ public class Trainee {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("How are you?");
+        System.out.println("Branch");
     }
 }
