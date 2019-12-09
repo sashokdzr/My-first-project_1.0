41ade8412fcbfc2d0a7e229d3fad2a364c1bf98d
sashokdzr <43587497+sashokdzr@users.noreply.github.com>
2019-12-09 22:26:26 +0300

    Delete Trainee.java

--- src/Trainee.java
+++
@@ -1,7 +0,0 @@
-public class Trainee {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("How are you?");
-        System.out.println("Branch");
-    }
-}
