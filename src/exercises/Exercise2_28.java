6c74d1c82a1763f267ad73432b488ab5bf76ffbd
sashokdzr <sashok200888@gmail.com>
2019-12-10 23:50:44 +0300

    # Conflicts:                     #	src/Trainee.java

---
+++ src/exercises/Exercise2_28.java
@@ -0,0 +1,14 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise2_28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi = Math.PI;
+        System.out.print("Enter raduis: ");
+        int r = input.nextInt();
+        System.out.printf("%s%d%n%s%f%n%s%f%n", "diameter=", 2 * r, "circumference=", 2 * pi * r, "area=", pi * r * r);
+
+    }
+}
