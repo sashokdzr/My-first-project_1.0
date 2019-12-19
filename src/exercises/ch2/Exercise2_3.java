f4bd241ff5e0fbadff37444e17b203c44093bbab
sashokdzr <sashok200888@gmail.com>
2019-12-20 00:03:06 +0300

    # Conflicts:                     #	src/Trainee.java

---
+++ src/exercises/ch2/Exercise2_3.java
@@ -0,0 +1,16 @@
+package exercises.ch2;
+
+import java.util.Scanner;
+public class Exercise2_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("First");
+        int x = input.nextInt();
+        System.out.println("Second");
+        int y = input.nextInt();
+        System.out.println("Third");
+        int z = input.nextInt();
+        int result = x + y + x;
+        System.out.printf("%s%n%d%n", "Product is:", result);
+    }
+}
--- src/theory/ch2/Welcome1.java
+++ src/theory/ch2/Welcome1.java
@@ -1,4 +1,4 @@
-package theory;
+package theory.ch2;
 
 public class Welcome1 { //class name
     public static void main(String[] args) {   //method name (main)
