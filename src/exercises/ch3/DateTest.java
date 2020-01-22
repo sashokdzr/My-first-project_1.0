c9d2114781904d50bde1571d9e2bedeafaf0c43e
sashokdzr <sashok200888@gmail.com>
2020-01-23 01:18:04 +0300

    # Conflicts:                     #	src/Trainee.java

--- src/exercises/ch3/DateTest.java
+++ src/exercises/ch3/DateTest.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 public class DateTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Date date1 = new Date(1, 21, 1999);
-        Date date2 = new Date(3, 4, 1988);
-        Date date3 = new Date(4, 5, -200);
-
-        date1.displayDate();
-
-        date2.displayDate();
-
-        date3.displayDate();
-
-
+        System.out.println("Input a month: ");
+        int m = input.nextInt();
+        System.out.println("Input a day: ");
+        int d = input.nextInt();
+        System.out.println("Input a year: ");
+        int y = input.nextInt();
+        Date cdate1 = new Date(m, d, y);
+        cdate1.displayDate(d, m, y);
     }
 
 }
