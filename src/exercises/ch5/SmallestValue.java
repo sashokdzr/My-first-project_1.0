7b1e5dfd66088d47f11366db3779c00f7df99d3e
sashokdzr <sashok200888@gmail.com>
2020-02-05 00:14:06 +0300

    # Conflicts:                     #	src/Trainee.java

--- src/exercises/ch5/SmallestValue.java
+++ src/exercises/ch5/SmallestValue.java
@@ -3,20 +3,52 @@ package exercises.ch5;
 public class SmallestValue {
     private int firstValue;
     private int value;
+    private int[] arr = new int[firstValue];
+    private int smallest;
 
     public SmallestValue(int firstValue, int value) {
         this.firstValue = firstValue;
         this.value = value;
     }
 
-    public void enter(int firstValue) {
+    public void enter() {
         int[] arr = new int[firstValue];
-        for (int i = 1; i <= firstValue; i++) {
+        for (int i = 0; i <= firstValue; i++) {
             arr[i] = value;
+        }
+        this.arr = arr;
+    }
+
+    public void smallest() {
+        smallest = arr[0];
+        for (int i = 1; i < firstValue; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
         }
     }
 
-    public void samllest() {
+    public void setFirstValue(int firstValue) {
+        this.firstValue = firstValue;
+    }
 
+    public int getFirstValue() {
+        return firstValue;
+    }
+
+    public int getSmallest() {
+        return smallest;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int[] getArr() {
+        return arr;
     }
 }
---
+++ src/exercises/ch5/SmallestValueTest.java
@@ -0,0 +1,12 @@
+package exercises.ch5;
+
+import java.util.Scanner;
+
+public class SmallestValueTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SmallestValue test;
+
+
+    }
+}
