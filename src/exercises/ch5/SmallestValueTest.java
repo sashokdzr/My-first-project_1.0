450519f5d2de2ad2424f13e0af52cfd03b93721d
sashokdzr <43587497+sashokdzr@users.noreply.github.com>
2020-02-05 14:12:08 +0300

    Update SmallestValueTest.java

--- src/exercises/ch5/SmallestValueTest.java
+++ src/exercises/ch5/SmallestValueTest.java
@@ -1,5 +1,3 @@
-package exercises.ch5;
-
 import java.util.Scanner;
 
 public class SmallestValueTest {
@@ -8,9 +6,8 @@ public class SmallestValueTest {
         System.out.print("How many elements in arr? ");
         SmallestValue test = new SmallestValue(input.nextInt());
         System.out.println();
-        System.out.printf("Elements in arr = %d", test.getFirstValue());
-        System.out.println(test.enter(test.getFirstValue(), input.nextInt()));
-
-
+        int n = test.getFirstValue();
+        System.out.printf("Elements in arr = %d%n", n );
+        System.out.printf("The smallest value is: %d", test.smallest(test.enter(n)));
     }
 }
