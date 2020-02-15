03e3239a7a8af3dc5e2fe8caf19f71ab269ce1e7
sashokdzr <sashok200888@gmail.com>
2020-02-16 00:31:48 +0300

    # Conflicts:                     #	src/Trainee.java
    Signed-off-by: sashokdzr <sashok200888@gmail.com>

---
+++ src/theory/ch3/Dialog1.java
@@ -0,0 +1,9 @@
+package theory.ch3;
+
+import javax.swing.*;
+
+public class Dialog1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to Java");
+    }
+}
